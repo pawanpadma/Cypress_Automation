fa996b008c0d44725799627b1207cfd4677948a7
Pawan <pawanpadma@gmail.com>
2020-08-30 19:49:40 -0400

    New Project
    New Project

---
+++ CypressAutomation/cypress/integration/smoke.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+import 'cypress-xpath';
+var username='input[name=email]';
+context('Utilities', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+  
+    it('My Application Demo', () => {
+        
+      // https://on.cypress.io/_
+      cy.get(username).type("rusdsev@yahoo.com");
+      cy.get('input[name=password]').type('$Blisdnk185');
+      cy.get('button[type=submit]').click();
+      cy.get('div[class*=MuiButtonBase-root]').trigger('mouseover');
+      cy.xpath('//div[@role="tooltip"]//div[text()="Leads"]').click();
+      //cy.get('div[role=tooltip]').click();
+      
+    })
+  
+    
+  
+    
+  
+    
+  })
